c5c2abf5d2e88824a9e63b633b68f223f70f5895
SeunAkanji <oluwaseunakanji42@gmail.com>
2022-09-08 12:47:56 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - C Printing
+ *
+ * Return: Always 0 (Excellent)
+ */
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
+
