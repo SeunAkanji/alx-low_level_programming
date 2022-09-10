dad18638017196ee64703765dfc662061675b198
SeunAkanji <oluwaseunakanji42@gmail.com>
2022-09-09 17:29:48 -0700

    The Alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Lowercase and Uppercase Letters
+ *
+ * Return: Always (Correct)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
+
