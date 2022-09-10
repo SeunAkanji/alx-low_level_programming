940464a39c39bf4abc66507f6ede25fcc24c5d35
SeunAkanji <oluwaseunakanji42@gmail.com>
2022-09-09 17:39:53 -0700

    prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Remove some letters
+ *
+ * Return: Always (Right)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
