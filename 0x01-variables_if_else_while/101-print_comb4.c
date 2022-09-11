d257029d7323846d7705bbabf02906fefca16545
SeunAkanji <oluwaseunakanji42@gmail.com>
2022-09-11 11:10:20 -0700

    Different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,6 +7,7 @@
 int main(void)
 {
 	int i, p, m;
+
 	for (i = 0; i <= 9; i++)
 	{
 		for (p = 1; p <= 9; p++)
@@ -18,7 +19,7 @@ int main(void)
 					putchar(i + '0');
 					putchar(p + '0');
 					putchar(m + '0');
-				         	if (i != 7)
+					if (i != 7)
 					{
 						putchar(',');
 						putchar(' ');
