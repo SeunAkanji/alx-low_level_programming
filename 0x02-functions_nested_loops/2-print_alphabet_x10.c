495e587aacc283a5127c760a288ce09d949bb538
SeunAkanji <oluwaseunakanji42@gmail.com>
2022-09-13 19:24:22 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - 10 times the alphabet
+ * @num1: the first integer
+ * @num2: the second integer entered
+ *
+ * Return: The sum of the integers
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
+
