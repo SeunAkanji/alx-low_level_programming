40bdde2463107511717dc56b0221b132ec5b7f8b
SeunAkanji <oluwaseunakanji42@gmail.com>
2022-09-15 16:06:56 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * main - prints whether or not a number is positive or not
+ * main - prints whether number is positive or not
  * Return: 0
  */
 
 int main(void)
 {
 	int i;
-	
+
 	i = 0;
 	positive_or_negative(i);
 
