d1c735d5d826825cc4758d77b517878f1a9dfe77
SeunAkanji <oluwaseunakanji42@gmail.com>
2022-10-06 15:19:29 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,62 +1,41 @@
 #include "main.h"
+
 /**
- * _strlen - count array
- * @s: array of elements
- * Return: i
+ * _memset - copy char
+ * @s: string
+ * @b: input
+ * @n: bytes
+ * Return: string
  */
 
-int _strlen(char *s)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	i = 0;
-	while (s[i] != '\0') /*Count character of string*/
+	for (i = 0; i < n; i++)
 	{
-		i++;
+		s[i] = b;
 	}
+	return (s);
 
-	return (i);
 }
 
 /**
- * str_concat - back a pointer to array
- * @s1: Array one
- * @s2: Array two
- * Return: Always an array dynamic
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: n elements
+ * @size: bytes
+ * Return: pointer
  */
-
-char *str_concat(char *s1, char *s2)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *dst;
-	unsigned int i, j, size;
+	void *p;
 
-	/*If the array is empty*/
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
-
-	/*count size total*/
-	size = (_strlen(s1) + _strlen(s2) + 1);
-
-	/*malloc*/
-	dst = (char *) malloc(size * sizeof(char));
-
-	if (dst == 0)
-	{
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-	}
+	p = malloc(nmemb * size);
 
-	/*Concatenate arrays*/
-	for (i = 0; *(s1 + i) != '\0'; i++)
-		*(dst + i) = *(s1 + i);
-
-	for (j = 0; *(s2 + j) != '\0'; j++)
-	{
-		*(dst + i) = *(s2 + j);
-		i++;
-	}
-
-	return (dst);
+	if (p == NULL)
+		return (NULL);
+	_memset(p, 0, (nmemb * size));
+	return (p);
 }
