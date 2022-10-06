cb1dd7d1239c64f7537c8c1d3b3936700a2847eb
SeunAkanji <oluwaseunakanji42@gmail.com>
2022-10-06 15:53:55 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -7,7 +7,7 @@
  * @new_size: new size
  * Return: pointer
  */
-void *_realloc(void *ptr, unsigned int old_size, usigned new_size)
+void *_realloc(void *ptr, unsigned int old_size, usigned int new_size)
 {
 	char *clone, *relloc;
 	unsigned int i;
@@ -31,7 +31,6 @@ void *_realloc(void *ptr, unsigned int old_size, usigned new_size)
 		*(relloc + i) = clone[i];
 	}
 	free(ptr);
-
-	return (relloc);
+return (relloc);
 }
 
