5de8d6c58230a77d22d9fbb29acea56237c2a679
SeunAkanji <oluwaseunakanji42@gmail.com>
2022-10-12 14:24:42 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,7 +10,7 @@
  * the cmp function does not return 0, or -1 if no match is found
  * or size is negative
  */
-int int_index(int *array, *array, int size, int (*cmp)(int))
+int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
